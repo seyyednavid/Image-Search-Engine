de18c7ff04f8b63ca928a895b84574cb577fca23
Navid Hejazi <navid.hejazi@Tensator.com>
2025-06-07 19:22:52 +0100

    bring in pre-trained model and save the model

--- 101_cnn_image_search_engine.py
+++ 101_cnn_image_search_engine.py
@@ -16,3 +16,26 @@ from os import listdir
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 import pickle
+
+
+###########################################################################################
+# bring in pre-trained model (excluding top)
+###########################################################################################
+
+# image parameters
+
+img_width = 224
+img_height = 224
+num_channels = 3
+
+
+# network architecture
+
+vgg = VGG16(input_shape=(img_width, img_height, num_channels), include_top = False, pooling = 'avg')
+
+
+model = Model(inputs = vgg.input, outputs = vgg.layers[-1].output)
+
+# save model file
+
+model.save("models/vgg16_search_engine.h5")
